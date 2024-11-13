95c2d92122eead7705eb4caa9b326db67464bc9a
ernestonsongo <106477881+ernest56-o@users.noreply.github.com>
2024-11-13 22:58:19 +0300

    Add files via upload

---
+++ answers.sql
@@ -0,0 +1,155 @@
+<<<<<<< HEAD
+CREATE DATABASE hospital_db;
+
+USE hospital_db;
+
+-- Create table for Patients
+CREATE TABLE patients (
+    patient_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    gender VARCHAR(10) NOT NULL,
+    language VARCHAR(20) NOT NULL
+);
+
+-- Create table for providers
+CREATE TABLE providers (
+    provider_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    provider_speciality VARCHAR(100) NOT NULL,
+    email_address VARCHAR(100),
+    phone_number VARCHAR(15),
+    date_joined DATE NOT NULL
+);
+
+-- Create table for visits
+CREATE TABLE visits (
+    visit_id INT PRIMARY KEY AUTO_INCREMENT,
+    patient_id INT,
+    provider_id INT,
+    date_of_visit DATE NOT NULL,
+    date_scheduled DATE NOT NULL,
+    visit_department_id INT NOT NULL,
+    visit_type VARCHAR(50) NOT NULL,
+    blood_pressure_systollic INT,
+    blood_pressure_diastollic DECIMAL,
+    pulse DECIMAL,
+    visit_status VARCHAR(50) NOT NULL,
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+    FOREIGN KEY (provider_id) REFERENCES providers(provider_id)
+);
+
+-- Create table for ed_visits
+CREATE TABLE ed_visits (
+    ed_visit_id INT PRIMARY KEY AUTO_INCREMENT,
+    visit_id INT,
+    patient_id INT,
+    acuity INT NOT NULL,
+    reason_for_visit VARCHAR(255) NOT NULL,
+    disposition VARCHAR(50) NOT NULL,
+    FOREIGN KEY (visit_id) REFERENCES visits(visit_id),
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id)
+);
+
+-- Create table for admissions
+CREATE TABLE admissions (
+    admission_id INT PRIMARY KEY AUTO_INCREMENT,
+    patient_id INT,
+    admission_date DATE NOT NULL,
+    discharge_date DATE NOT NULL,
+    discharge_disposition VARCHAR(50) NOT NULL,
+    service VARCHAR(100) NOT NULL,
+    primary_diagnosis VARCHAR(255) NOT NULL,
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id)
+);
+
+-- Create table for discharges
+CREATE TABLE discharges (
+    discharge_id INT PRIMARY KEY AUTO_INCREMENT,
+    admission_id INT,
+    patient_id INT,
+    discharge_date DATE NOT NULL,
+    discharge_disposition VARCHAR(50) NOT NULL,
+    FOREIGN KEY (admission_id) REFERENCES admissions(admission_id),
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id)
+);
+=======
+CREATE DATABASE hospital_db;
+
+USE hospital_db;
+
+-- Create table for Patients
+CREATE TABLE patients (
+    patient_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    gender VARCHAR(10) NOT NULL,
+    language VARCHAR(20) NOT NULL
+);
+
+-- Create table for providers
+CREATE TABLE providers (
+    provider_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    provider_speciality VARCHAR(100) NOT NULL,
+    email_address VARCHAR(100),
+    phone_number VARCHAR(15),
+    date_joined DATE NOT NULL
+);
+
+-- Create table for visits
+CREATE TABLE visits (
+    visit_id INT PRIMARY KEY AUTO_INCREMENT,
+    patient_id INT,
+    provider_id INT,
+    date_of_visit DATE NOT NULL,
+    date_scheduled DATE NOT NULL,
+    visit_department_id INT NOT NULL,
+    visit_type VARCHAR(50) NOT NULL,
+    blood_pressure_systollic INT,
+    blood_pressure_diastollic DECIMAL,
+    pulse DECIMAL,
+    visit_status VARCHAR(50) NOT NULL,
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+    FOREIGN KEY (provider_id) REFERENCES providers(provider_id)
+);
+
+-- Create table for ed_visits
+CREATE TABLE ed_visits (
+    ed_visit_id INT PRIMARY KEY AUTO_INCREMENT,
+    visit_id INT,
+    patient_id INT,
+    acuity INT NOT NULL,
+    reason_for_visit VARCHAR(255) NOT NULL,
+    disposition VARCHAR(50) NOT NULL,
+    FOREIGN KEY (visit_id) REFERENCES visits(visit_id),
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id)
+);
+
+-- Create table for admissions
+CREATE TABLE admissions (
+    admission_id INT PRIMARY KEY AUTO_INCREMENT,
+    patient_id INT,
+    admission_date DATE NOT NULL,
+    discharge_date DATE NOT NULL,
+    discharge_disposition VARCHAR(50) NOT NULL,
+    service VARCHAR(100) NOT NULL,
+    primary_diagnosis VARCHAR(255) NOT NULL,
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id)
+);
+
+-- Create table for discharges
+CREATE TABLE discharges (
+    discharge_id INT PRIMARY KEY AUTO_INCREMENT,
+    admission_id INT,
+    patient_id INT,
+    discharge_date DATE NOT NULL,
+    discharge_disposition VARCHAR(50) NOT NULL,
+    FOREIGN KEY (admission_id) REFERENCES admissions(admission_id),
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id)
+);
+>>>>>>> 07ea9e7821dabd8dcd83d23f16b74bd5beb35007
